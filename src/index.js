825790eaf77d45992e723253a9eea58633b7ebe0
aiden <aiden.eckstrom@gmail.com>
2023-02-10 17:12:37 +0000

    First commit

---
+++ src/index.js
@@ -0,0 +1,114 @@
+import createBareServer from "@tomphttp/bare-server-node";
+import subdomain from 'express-subdomain'
+import express from "express";
+const router = express.Router()
+import { createServer } from "node:http";
+//import { publicPath } from "ultraviolet-static";
+//import { publicPath } from "./ultraviolet-static/lib"
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { join, resolve } from "node:path";
+import { readFile } from 'fs/promises';
+import { hostname } from "node:os";
+import basicAuth from 'express-basic-auth'
+const publicPath = resolve('./src/ultraviolet-static/public')
+
+const bare = createBareServer("/bare/");
+const app = express();
+
+
+const json = JSON.parse(
+  await readFile(
+    new URL('./users.json', import.meta.url)
+  )
+);
+console.log(json)
+app.use(basicAuth({
+    users: json,
+	challenge: true,
+	authorizer: myAuthorizer,
+    unauthorizedResponse: getUnauthorizedResponse	
+}))
+
+function myAuthorizer(username, password) {
+	let keys  = Object.keys(json)
+	// iterate over each element in the array
+	for (var i = 0; i < keys.length; i++){
+	  // look for the entry with a matching `code` value
+		const userMatches = basicAuth.safeCompare(username, keys[i])
+		if (userMatches){
+			// we found it
+	    	// obj[i].name is the matched result
+			const passwordMatches = basicAuth.safeCompare(password, json[username])
+			if (userMatches && passwordMatches){
+				// console.log(username, "  ", password, " has logged in")
+			}
+    		return userMatches & passwordMatches
+		}
+	}
+    
+}
+
+
+
+function getUnauthorizedResponse(req) {
+    return req.auth
+        ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
+        : 'No credentials provided'
+}
+
+
+
+
+
+// Load our publicPath first and prioritize it over UV.
+app.use(express.static(publicPath));
+// Load vendor files last.
+// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
+app.use("/uv/", express.static(uvPath));
+
+// Error for everything else
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(join(publicPath, "404.html"));
+});
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+server.listen({
+  port,
+});
